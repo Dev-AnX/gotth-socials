578eefd7b7f60fa3ea4b181e94781cb24117e5ee
Renan Godói <renanpgodoi@gmail.com>
2025-09-24 12:36:25 -0300

    Adicionando variaveis, if/else e função

--- main.go
+++ main.go
@@ -4,5 +4,21 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, Gotth Stack!")
+	nome := "Mundo"
+	ano := 2025
+
+	fmt.Printf("Olá, %s! Estamos no ano de %d! \n", nome, ano)
+
+	fds := false
+
+	if fds {
+		fmt.Println("Opa, chegou o fim de semana! Hora de descansar.")
+	} else {
+		fmt.Println("Foco! Ainda é dia de semana.")
+	}
+	dizOla()
+}
+
+func dizOla() {
+	fmt.Println("Olá de novo, Renan!")
 }
